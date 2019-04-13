886bf70381fa8a66115732faf45e6f81ccf1a207
Greg Lawrence <bluhurr@gmail.com>
2019-04-13 18:54:35 -0400

    Comment edit

--- openBlackboard_Template.py
+++ openBlackboard_Template.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 # Open instance of Firefox, maximize the window, and open up Blackboard Sign
 # in page.
-# Change Firefox to Chrome or whatever as needed. (I have no clue how well)
+# Change Firefox to Chrome or whatever as needed. (I have no clue how well other browsers work)
 driver = webdriver.Firefox()
 driver.maximize_window()
 driver.get("https://lowell.umassonline.net/webapps/bb-auth-provider-shibboleth-BBLEARN/execute/shibbolethLogin?returnUrl=https%3A%2F%2Flowell.umassonline.net%2Fwebapps%2Fportal%2Fexecute%2FdefaultTab&authProviderId=_8264_1")
